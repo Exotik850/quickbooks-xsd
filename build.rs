040399375cfed7ae118fa4859b9fb91703769805
Exotik850 <kidkool850@gmail.com>
2026-05-18 18:16:11 -0500

    fix duplicated code generation, start configuring features

--- build.rs
+++ build.rs
@@ -1,12 +1,14 @@
+#[cfg(feature = "generate")]
 use std::{
     io::Write,
     path::PathBuf,
     process::{Command, Output, Stdio},
 };
-
+#[cfg(feature = "generate")]
 use xsd_parser::{
     Config,
-    config::{GeneratorFlags, InterpreterFlags, OptimizerFlags, ParserConfig, ParserFlags, Schema},
+    config::{GeneratorFlags, InterpreterFlags, OptimizerFlags, ParserFlags, Schema},
+    models::ExplicitNaming,
 };
 
 fn main() {
@@ -14,9 +16,13 @@ fn main() {
     build_schema_file();
 }
 
+#[cfg(feature = "generate")]
 fn build_schema_file() {
+
     let cargo_manifest_dir: PathBuf = std::env::var("CARGO_MANIFEST_DIR").unwrap().into();
     let xsd_dir = cargo_manifest_dir.join("xsd");
+    let out_dir: PathBuf = std::env::var("OUT_DIR").unwrap().into();
+
     println!("cargo:rerun-if-changed={}", xsd_dir.display());
 
     // xsd has folders for the different versions of the schema,
@@ -41,7 +47,7 @@ fn build_schema_file() {
         })
         .max_by(|a, b| a.0.cmp(&b.0))
         .map(|(version, path)| {
-            println!("Found version: {}", version);
+            println!("Found version: {version}");
             path
         })
         .unwrap();
@@ -63,22 +69,22 @@ fn build_schema_file() {
         .with_schemas(xsd_files.into_iter().map(Schema::File))
         .with_generator_flags(
             GeneratorFlags::all()
-                ^ GeneratorFlags::BUILD_IN_ABSOLUTE_PATHS
-                ^ GeneratorFlags::ABSOLUTE_PATHS_INSTEAD_USINGS
+                ^ GeneratorFlags::ANY_TYPE_SUPPORT
                 ^ GeneratorFlags::ADVANCED_ENUMS,
         )
-        .with_interpreter_flags(InterpreterFlags::all())
+        .with_naming(ExplicitNaming::default())
+        .with_interpreter_flags(InterpreterFlags::all() ^ InterpreterFlags::WITH_NUM_BIG_INT)
         .with_parser_flags(ParserFlags::all())
         .with_optimizer_flags(OptimizerFlags::all() ^ OptimizerFlags::FLATTEN_COMPLEX_TYPES)
         .with_quick_xml();
     let code = xsd_parser::generate(config).expect("Could not generate schemas");
     let code = code.to_string();
     let code = rustfmt_pretty_print(code).expect("Could not format generated code");
-    let out_dir: PathBuf = std::env::var("OUT_DIR").unwrap().into();
     std::fs::write(out_dir.join("schemas.rs"), code)
         .expect("Could not write generated code to file");
 }
 
+#[cfg(feature = "generate")]
 // A small helper to call `rustfmt` when generating file(s).
 // This may be useful to compare different versions of generated files.
 pub fn rustfmt_pretty_print(code: String) -> Result<String, xsd_parser::Error> {
@@ -107,9 +113,10 @@ pub fn rustfmt_pretty_print(code: String) -> Result<String, xsd_parser::Error> {
         let code = status.code();
         match code {
             Some(code) => {
-                if code != 0 {
-                    panic!("The `rustfmt` command failed with return code {code}!\n{stderr}");
-                }
+                assert!(
+                    code == 0,
+                    "The `rustfmt` command failed with return code {code}!\n{stderr}"
+                );
             }
             None => {
                 panic!("The `rustfmt` command failed!\n{stderr}")
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
 //! # quickbooks-xsd
-//! 
+//!
 //! XSD Types for the Quickbooks QBO API generated for Rust
-//! 
+
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(nonstandard_style)]
 
 #[cfg(feature = "generate")]
 include!(concat!(env!("OUT_DIR"), "/schemas.rs"));
 
 #[cfg(not(feature = "generate"))]
-mod schemas;
+include!("schemas.rs");
