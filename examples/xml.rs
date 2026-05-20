6ab6791d1b983f9555741be86a6765c93e5f3724
kidkool850@gmail.com <kidkool850@gmail.com>
2026-05-19 20:08:46 -0500

    add readme to docs, clean up for initial release

--- examples/xml.rs
+++ examples/xml.rs
@@ -1,16 +1,13 @@
 use quickbooks_xsd::finance::InvoiceType;
-// use serde::Deserialize;
 use xsd_parser_types::quick_xml::{self, DeserializeSync, XmlReader};
 
 fn main() {
-    // let xml = include_str!("./invoice.xml");
     let xml = std::fs::OpenOptions::new()
         .read(true)
         .open("./examples/invoice.xml")
         .unwrap();
     let reader = std::io::BufReader::new(xml);
     let mut reader = quick_xml::reader::IoReader::new(reader).with_error_info();
-    // let mut reader = quick_xml::reader::SliceReader::new(xml);
     let parsed = InvoiceType::deserialize(&mut reader).unwrap();
     println!("{:#?}", parsed);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,5 @@
-//! # quickbooks-xsd
-//!
-//! XSD Types for the Quickbooks QBO API generated for Rust
-
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(nonstandard_style)]
-#![allow(dead_code)]
-#![allow(unused_variables)]
+#![doc = include_str!("../README.md")]
+#![allow(dead_code, unused_variables, unused_mut)]
 
 pub use xsd_parser_types as types;
 
